213854932fdae4acb686b5af99aa3f3a4c6ff59c
Samy Pessé <samypesse@gmail.com>
2014-07-21 14:17:33 -0700

    Add loading indicator to statusbar

--- src/index.js
+++ src/index.js
@@ -11,45 +11,62 @@ define([
     var rpc = codebox.require("core/rpc");
     var hash = codebox.require("utils/hash");
 
+    // Return packages from repository
+    var listFromRepository = function() {
+        var p = hr.Requests.getJSON("https://api.github.com/repos/"+settings.data.get("repository")+"/readme")
+        .get("content")
+        .then(hash.atob)
+        .then(function(content) {
+            return _.chain(content.split("\n"))
+            .map(function(line) {
+                var parts = _.compact(line.split("|"));
+                if (parts.length != 4) return null;
+
+                return {
+                    'name': parts[0].trim(),
+                    'description': parts[1].trim(),
+                    'author': parts[2].trim(),
+                    'repository': parts[3].trim()
+                };
+            })
+            .compact()
+            .slice(2)
+            .value()
+        });
+
+        return codebox.statusbar.loading(p, {
+            prefix: "Loading repository"
+        });
+    };
+
+    // Install a package
+    var installPackage = function(url) {
+        var p = rpc.execute("packages/install", {
+            'url': url
+        })
+        .then(function() {
+            return dialogs.alert("This package will be fully operational once you reopen Codebox.")
+        });
+
+        return codebox.statusbar.loading(p, {
+            prefix: "Installing package"
+        });
+    };
+
     // Install a package from repository
     commands.register({
         id: "package.install",
         title: "Package Control: Install Package",
         run: function() {
-            return hr.Requests.getJSON("https://api.github.com/repos/"+settings.data.get("repository")+"/readme")
-            .get("content")
-            .then(hash.atob)
-            .then(function(content) {
-                return _.chain(content.split("\n"))
-                .map(function(line) {
-                    var parts = _.compact(line.split("|"));
-                    if (parts.length != 4) return null;
-
-                    return {
-                        'name': parts[0].trim(),
-                        'description': parts[1].trim(),
-                        'author': parts[2].trim(),
-                        'repository': parts[3].trim()
-                    };
-                })
-                .compact()
-                .slice(2)
-                .value()
-            })
+            return listFromRepository()
             .then(function(packages) {
                 return dialogs.list(packages, {
                     template: packageTemplate,
                     placeholder: "Install a package"
                 });
             })
-            .then(function(pkg) {
-                return rpc.execute("packages/install", {
-                    url: pkg.get("repository")
-                });
-            })
-            .then(function() {
-                return dialogs.alert("This package will be fully operational once you reopen Codebox.")
-            });
+            .get("repository")
+            .then(installPackage);
         }
     });
 
