7fdbeee1676d79b27d67b21d547ca1507b299db9
Samy Pessé <samypesse@gmail.com>
2015-04-08 12:44:00 +0200

    Adapt build for browserify

--- src/index.js
+++ src/index.js
@@ -1,113 +1,114 @@
-define([
-    "src/settings",
-    "text!src/templates/package.html",
-    "less!src/stylesheets/main.less"
-], function(settings, packageTemplate) {
-    var _ = codebox.require("hr/utils");
-    var hr = codebox.require("hr/hr");
-    var commands = codebox.require("core/commands");
-    var dialogs = codebox.require("utils/dialogs");
-    var packages = codebox.require("core/packages");
-    var rpc = codebox.require("core/rpc");
-    var hash = codebox.require("utils/hash");
+require("./stylesheets/main.less");
 
-    // Return packages from repository
-    var listFromRepository = function() {
-        var p = hr.Requests.getJSON("https://api.github.com/repos/"+settings.data.get("repository")+"/readme")
-        .get("content")
-        .then(hash.atob)
-        .then(function(content) {
-            return _.chain(content.split("\n"))
-            .map(function(line) {
-                var parts = _.compact(line.split("|"));
-                if (parts.length != 4) return null;
+var settings = require("./settings");
+var packageTemplate = require("./templates/package.html");
 
-                return {
-                    'name': parts[0].trim(),
-                    'description': parts[1].trim(),
-                    'author': parts[2].trim(),
-                    'repository': parts[3].trim()
-                };
-            })
-            .compact()
-            .slice(2)
-            .value()
-        });
+var Q = codebox.require("q");
+var _ = codebox.require("hr.utils");
+var axios = require("axios");
+var commands = codebox.require("core/commands");
+var dialogs = codebox.require("utils/dialogs");
+var packages = codebox.require("core/packages");
+var rpc = codebox.require("core/rpc");
+var hash = codebox.require("utils/hash");
 
-        return codebox.statusbar.loading(p, {
-            prefix: "Loading repository"
-        });
-    };
+// Return packages from repository
+var listFromRepository = function() {
+    var p = Q(axios.get("https://api.github.com/repos/"+settings.data.get("repository")+"/readme"))
+    .get("data")
+    .get("content")
+    .then(hash.atob)
+    .then(function(content) {
+        return _.chain(content.split("\n"))
+        .map(function(line) {
+            var parts = _.compact(line.split("|"));
+            if (parts.length != 4) return null;
 
-    // Install a package
-    var installPackage = function(url) {
-        var p = rpc.execute("packages/install", {
-            'url': url
-        });
-
-        return codebox.statusbar.loading(p, {
-            prefix: "Installing package"
+            return {
+                'name': parts[0].trim(),
+                'description': parts[1].trim(),
+                'author': parts[2].trim(),
+                'repository': parts[3].trim()
+            };
         })
-        .then(function() {
-            return dialogs.alert("Package '"+url+"' is now installed.")
-        })
-        .fail(dialogs.error);
-    };
-
-    // Uninstall a package
-    var uninstallPackage = function(name) {
-        var p = rpc.execute("packages/uninstall", {
-            'name': name
-        });
-
-        return codebox.statusbar.loading(p, {
-            prefix: "Removing package"
-        })
-        .then(function() {
-            return dialogs.alert("This package will be fully removed once you restart Codebox.")
-        })
-        .fail(dialogs.error);
-    };
-
-    // List packages
-    commands.register({
-        id: "package.list.all",
-        title: "Package Control: List Packages",
-        run: function() {
-            return dialogs.list(packages, {
-                template: packageTemplate
-            });
-        }
+        .compact()
+        .slice(2)
+        .value()
     });
 
-    // Install a package from repository
-    commands.register({
-        id: "package.install",
-        title: "Package Control: Install Package",
-        run: function() {
-            return listFromRepository()
-            .then(function(packages) {
-                return dialogs.list(packages, {
-                    template: packageTemplate,
-                    placeholder: "Install a package"
-                });
-            })
-            .post("get", ["repository"])
-            .then(installPackage);
-        }
+    return codebox.statusbar.loading(p, {
+        prefix: "Loading repository"
+    });
+};
+
+// Install a package
+var installPackage = function(url) {
+    var p = rpc.execute("packages/install", {
+        'url': url
     });
 
-    // Uninstall a package
-    commands.register({
-        id: "package.uninstall",
-        title: "Package Control: Remove Package",
-        run: function() {
+    return codebox.statusbar.loading(p, {
+        prefix: "Installing package"
+    })
+    .then(function() {
+        return dialogs.alert("Package '"+url+"' is now installed.")
+    })
+    .fail(dialogs.error);
+};
+
+// Uninstall a package
+var uninstallPackage = function(name) {
+    var p = rpc.execute("packages/uninstall", {
+        'name': name
+    });
+
+    return codebox.statusbar.loading(p, {
+        prefix: "Removing package"
+    })
+    .then(function() {
+        return dialogs.alert("This package will be fully removed once you restart Codebox.")
+    })
+    .fail(dialogs.error);
+};
+
+// List packages
+commands.register({
+    id: "package.list.all",
+    title: "Package Control: List Packages",
+    run: function() {
+        return dialogs.list(packages, {
+            template: packageTemplate
+        });
+    }
+});
+
+// Install a package from repository
+commands.register({
+    id: "package.install",
+    title: "Package Control: Install Package",
+    run: function() {
+        return listFromRepository()
+        .then(function(packages) {
             return dialogs.list(packages, {
                 template: packageTemplate,
-                placeholder: "Remove a package"
-            })
-            .post("get", ["name"])
-            .then(uninstallPackage);
-        }
-    });
+                placeholder: "Install a package"
+            });
+        })
+        .post("get", ["repository"])
+        .then(installPackage);
+    }
+});
+
+// Uninstall a package
+commands.register({
+    id: "package.uninstall",
+    title: "Package Control: Remove Package",
+    run: function() {
+        return dialogs.list(packages, {
+            template: packageTemplate,
+            placeholder: "Remove a package"
+        })
+        .post("get", ["name"])
+        .then(uninstallPackage);
+    }
 });
--- src/settings.js
+++ src/settings.js
@@ -1,15 +1,13 @@
-define(function() {
-    return codebox.settings.schema("packages",
-        {
-            "title": "Package Control",
-            "type": "object",
-            "properties": {
-                "repository": {
-                    "description": "Repository",
-                    "type": "string",
-                    "default": "CodeboxIDE/packages"
-                }
+module.exports = codebox.settings.schema("packages",
+    {
+        "title": "Package Control",
+        "type": "object",
+        "properties": {
+            "repository": {
+                "description": "Repository",
+                "type": "string",
+                "default": "CodeboxIDE/packages"
             }
         }
-    );
-});
+    }
+);
